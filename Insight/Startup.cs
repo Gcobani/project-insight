f1fed5f940027fa958d17fe68702fd77dcf0e414
Gcwn <s210139889@nmmu.ac.za>
2015-07-23 12:09:55 +0200

    init

---
+++ Insight/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Insight.Startup))]
+namespace Insight
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
